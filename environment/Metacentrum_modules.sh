c9b5aed362b7fa32eb277daf81a369301b435e22
Jan Vabek <vabekjan@gmail.com>
2025-02-04 18:03:47 +0100

    HPC clusters: Metacentrum compilation

---
+++ environment/Metacentrum_modules.sh
@@ -0,0 +1,7 @@
+module purge
+module add boost/1.79.0-gcc-10.2.1-4pgmuar
+module add fftw/3.3.10-gcc-10.2.1-yxsjm6z hdf5/1.12.2-gcc-10.2.1-gfdwqr3
+module add gettext/0.20.2-gcc
+
+export HDF5_ROOT_DIR=/cvmfs/software.metacentrum.cz/spack18/software/linux-debian11-x86_64_v2/gcc-10.2.1/hdf5-1.12.2-gfdwqr3fdxejqi4ykiv7ammx4cmxmm7c/
+export BOOST_ROOT_DIR=/cvmfs/software.metacentrum.cz/spack18/software/linux-debian11-x86_64_v2/gcc-10.2.1/boost-1.79.0-4pgmuar4icgafmrdpfn6pqmhidbtrnhy/
